4517ad3651e50a263232f342c1d1e6f49c414156
Svetlana Dyakova <dyakova030@gmail.com>
2026-05-07 16:17:55 +0300

    Добавить регистрацию пользователей и настройки редиректов

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("pages/", include("pages.urls")),
+    path("auth/", include("django.contrib.auth.urls")),
+    path(
+        "auth/registration/",
+        CreateView.as_view(
+            template_name="registration/registration_form.html",
+            form_class=UserCreationForm,
+            success_url=reverse_lazy("blog:index"),
+        ),
+        name="registration",
+    ),
     path("", include("blog.urls")),
 ]
 
