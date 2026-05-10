5d35ad38ec14712ded075ff3111c458e2f24df36
Svetlana Dyakova <dyakova030@gmail.com>
2026-05-10 15:39:19 +0300

    Добавить is_published и format в PostForm

--- blogicum/blog/forms.py
+++ blogicum/blog/forms.py
@@ -15,9 +15,19 @@ class UserEditForm(forms.ModelForm):
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ["title", "text", "pub_date", "location", "category", "image"]
+        fields = [
+            "title",
+            "text",
+            "pub_date",
+            "location",
+            "category",
+            "image",
+            "is_published",
+        ]
         widgets = {
-            "pub_date": forms.DateTimeInput(attrs={"type": "datetime-local"}),
+            "pub_date": forms.DateTimeInput(
+                attrs={"type": "datetime-local"}, format="%Y-%m-%d %H:%M:%S"
+            ),
         }
 
 
