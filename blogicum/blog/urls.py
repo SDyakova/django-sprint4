5936a8447ac7c87fb23e029274eb1a726b62f527
Svetlana Dyakova <dyakova030@gmail.com>
2026-05-12 16:32:08 +0300

    Рефакторинг: вынести маршруты posts в отдельный список

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -1,8 +1,25 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 app_name = "blog"
+
+posts_urls = [
+    path(
+        "<int:post_id>/edit_comment/<int:comment_id>/",
+        views.edit_comment,
+        name="edit_comment",
+    ),
+    path(
+        "<int:post_id>/delete_comment/<int:comment_id>/",
+        views.delete_comment,
+        name="delete_comment",
+    ),
+    path("<int:post_id>/edit/", views.edit_post, name="edit_post"),
+    path("<int:post_id>/delete/", views.delete_post, name="delete_post"),
+    path("<int:post_id>/comment/", views.add_comment, name="add_comment"),
+    path("<int:post_id>/", views.post_detail, name="post_detail"),
+]
 
 urlpatterns = [
     path("", views.index, name="index"),
@@ -11,23 +28,8 @@ urlpatterns = [
         views.category_posts,
         name="category_posts",
     ),
-    path(
-        "posts/<int:post_id>/edit_comment/<int:comment_id>/",
-        views.edit_comment,
-        name="edit_comment",
-    ),
-    path(
-        "posts/<int:post_id>/delete_comment/<int:comment_id>/",
-        views.delete_comment,
-        name="delete_comment",
-    ),
-    path("posts/<int:post_id>/edit/", views.edit_post, name="edit_post"),
-    path("posts/<int:post_id>/delete/", views.delete_post, name="delete_post"),
-    path(
-        "posts/<int:post_id>/comment/", views.add_comment, name="add_comment"
-    ),
-    path("posts/<int:post_id>/", views.post_detail, name="post_detail"),
     path("profile/edit/", views.edit_profile, name="edit_profile"),
     path("profile/<str:username>/", views.profile, name="profile"),
     path("posts/create/", views.create_post, name="create_post"),
+    path("posts/", include(posts_urls)),
 ]
